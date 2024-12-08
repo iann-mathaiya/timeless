cca88a8416b47da7420e2c5a827c7647640c8f9a
Ian Mathaiya <iannmathaiya@gmail.com>
2024-12-08 21:09:24 +0300

    configured astro env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react()],
 	experimental: {
 		svg: true,
+	},
+	env: {
+		schema: {
+			BETTER_AUTH_SECRET: envField.string({ context: "server", access: "secret" }),
+		},
 	},
 });
--- env.d.ts
+++ env.d.ts
@@ -1,18 +1,18 @@
- 
+
 /// <reference path="../.astro/types.d.ts" />
- 
+
 declare namespace App {
     // Note: 'import {} from ""' syntax does not work in .d.ts files.
     interface Locals {
-        user: import("better-auth").User | null;
-        session: import("better-auth").Session | null;
+        user: import("better-auth").User | null
+        session: import("better-auth").Session | null
     }
 }
 
 interface ImportMetaEnv {
-    readonly BETTER_AUTH_SECRET: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+    readonly BETTER_AUTH_SECRET: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
