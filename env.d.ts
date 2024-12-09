208b1f284898d8346816be7605e8bf4062858a31
Ian Mathaiya <iannmathaiya@gmail.com>
2024-12-09 21:44:00 +0300

    extended Locals with cloudflare Runtime type

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,11 @@
 
 /// <reference path="../.astro/types.d.ts" />
 
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
 declare namespace App {
     // Note: 'import {} from ""' syntax does not work in .d.ts files.
-    interface Locals {
+    interface Locals extends Runtime {
         user: import("better-auth").User | null
         session: import("better-auth").Session | null
         db: ReturnType<typeof import('./src/db').createD1Connection>
