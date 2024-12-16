70d6ff10e6b46761a180c2c92b9bc3f749057162
Ian Mathaiya <iannmathaiya@gmail.com>
2024-12-16 20:00:55 +0300

    added a tooltip to the image upload btn

--- src/components/create-post-form.tsx
+++ src/components/create-post-form.tsx
@@ -1,8 +1,9 @@
 import { useRef, useState } from 'react';
 import { ImagePlus } from 'lucide-react';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
 
 export default function CreatePostForm() {
-    const [images, setImages] = useState<string[]>([])
+    const [images, setImages] = useState<string[]>([]);
     const inputRef = useRef<HTMLInputElement>(null);
 
     const url = 'https://d2wx6rahy8yxgr.cloudfront.net/fit-in/2560x0/filters:format(webp)/filters:quality(100)/8a946048-8015-4fc3-8173-31961d6f78b4-send-us-a-message.png';
@@ -16,9 +17,9 @@ export default function CreatePostForm() {
         // use the file
         console.log(file);
 
-        setImages(prevImages => [...prevImages, url])
+        setImages(prevImages => [...prevImages, url]);
 
-        console.log(images)
+        console.log(images);
 
     }
 
@@ -36,20 +37,29 @@ export default function CreatePostForm() {
             <textarea placeholder='Write something...' className='mt-2 min-h-20 w-full text-sm outline-none' />
 
             {images.length > 0 &&
-            <div className='flex items-center gap-2'>
-                {images.map((image, index) => 
-                    <img key={`${image}-${index}`} src={image} alt='Just a placeholder' className='w-80 rounded-md' />
-                )}
-            </div>
+                <div className='flex items-center gap-2'>
+                    {images.map((image, index) =>
+                        <img key={`${image}-${index}`} src={image} alt='Just a placeholder' className='w-80 rounded-md' />
+                    )}
+                </div>
             }
             <div className='mt-4 w-full flex items-center gap-2'>
 
                 <div className='w-full flex items-center justify-between'>
                     <div className='flex items-center justify-center'>
-                        <input ref={inputRef} type='file' onChange={handleFileUpload} className='hidden' />
-                        <button type="button" onClick={handleAddImage} className='p-1 text-gray-600 hover:text-gray-900 bg-transparent hover:bg-gray-200 rounded-md'>
-                            <ImagePlus className='size-5' />
-                        </button>
+                        <input ref={inputRef} type='file' accept='image/webp, image/jpeg, image/png' onChange={handleFileUpload} className='hidden' />
+                        <TooltipProvider delayDuration={400} skipDelayDuration={150}>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <button type="button" onClick={handleAddImage} className='p-1 text-gray-600 hover:text-gray-900 bg-transparent hover:bg-gray-200 rounded-md'>
+                                        <ImagePlus className='size-5' />
+                                    </button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>Add an image</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
                     </div>
 
                     <button
