657a36753c2f4c4a7bc9ddff6d6684987f2a8eed
Ian Mathaiya <iannmathaiya@gmail.com>
2024-12-16 19:43:34 +0300

    updated the create post form

--- src/components/create-post-form.tsx
+++ src/components/create-post-form.tsx
@@ -1,12 +1,11 @@
-import { useRef } from 'react';
-import { useDebouncedCallback } from 'use-debounce';
-import { ImagePlus, ListTodo, MapPinned } from 'lucide-react';
+import { useRef, useState } from 'react';
+import { ImagePlus } from 'lucide-react';
 
 export default function CreatePostForm() {
+    const [images, setImages] = useState<string[]>([])
     const inputRef = useRef<HTMLInputElement>(null);
 
     const url = 'https://d2wx6rahy8yxgr.cloudfront.net/fit-in/2560x0/filters:format(webp)/filters:quality(100)/8a946048-8015-4fc3-8173-31961d6f78b4-send-us-a-message.png';
-
 
     function handleFileUpload(e: React.ChangeEvent<HTMLInputElement>) {
         const files = e.target.files;
@@ -16,6 +15,10 @@ export default function CreatePostForm() {
 
         // use the file
         console.log(file);
+
+        setImages(prevImages => [...prevImages, url])
+
+        console.log(images)
 
     }
 
@@ -27,18 +30,38 @@ export default function CreatePostForm() {
     }
 
     return (
-        <div className='mt-4 mx-auto w-full max-w-xl'>
+        <form className='mt-4 mx-auto w-full max-w-xl'>
             <input placeholder='Add title' className='w-full text-xl font-medium bg-transparent outline-none placeholder:text-gray-400' />
 
+            <textarea placeholder='Write something...' className='mt-2 min-h-20 w-full text-sm outline-none' />
+
+            {images.length > 0 &&
+            <div className='flex items-center gap-2'>
+                {images.map((image, index) => 
+                    <img key={`${image}-${index}`} src={image} alt='Just a placeholder' className='w-80 rounded-md' />
+                )}
+            </div>
+            }
             <div className='mt-4 w-full flex items-center gap-2'>
 
-                <div className='flex items-center justify-center'>
-                    <input ref={inputRef} type='file' onChange={handleFileUpload} className='hidden' />
-                    <button type="button" onClick={handleAddImage} className='p-1 text-gray-600 hover:text-gray-900 bg-transparent hover:bg-gray-200 rounded-md'>
-                        <ImagePlus className='size-5' />
+                <div className='w-full flex items-center justify-between'>
+                    <div className='flex items-center justify-center'>
+                        <input ref={inputRef} type='file' onChange={handleFileUpload} className='hidden' />
+                        <button type="button" onClick={handleAddImage} className='p-1 text-gray-600 hover:text-gray-900 bg-transparent hover:bg-gray-200 rounded-md'>
+                            <ImagePlus className='size-5' />
+                        </button>
+                    </div>
+
+                    <button
+                        type='submit'
+                        className='px-3 py-1.5 text-white text-sm bg-gray-950 hover:bg-orange-600 rounded-full'
+                    >
+                        Save post
                     </button>
                 </div>
+
+
             </div>
-        </div>
+        </form>
     );
 }
