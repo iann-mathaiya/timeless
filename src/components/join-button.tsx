c47a7a9de9be1b5ef36b4a80cfa9ddb6c10bed11
Ian Mathaiya <iannmathaiya@gmail.com>
2025-01-06 09:53:10 +0300

    styling: polish landing page, join and sign in backgrounds

--- src/components/join-button.tsx
+++ src/components/join-button.tsx
@@ -11,7 +11,7 @@ export default function JoinButton() {
                     callbackURL: "/home",
                 })
             }}
-            className="mt-4 w-full h-8 flex items-center justify-center gap-2 text-white bg-gray-950 hover:bg-orange-600 rounded-lg transition-all duration-300 ease-in-out">
+            className="mt-2 w-full h-8 flex items-center justify-center gap-2 text-white bg-gray-950 hover:bg-orange-600 rounded-lg transition-all duration-300 ease-in-out">
             <GoogleLogo className="size-4" />
             <span className="text-sm font-medium">
                 Sign up with Google
