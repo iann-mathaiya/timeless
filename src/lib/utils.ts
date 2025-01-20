89ccdc7315fffce54f0ac3ffecfd09185983d9fd
Ian Mathaiya <iannmathaiya@gmail.com>
2025-01-20 21:03:59 +0300

    styling: added formatted friend's post date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,7 @@ import fs from "node:fs"
 import path from "node:path"
 import { twMerge } from "tailwind-merge"
 import { clsx, type ClassValue } from "clsx"
-import { z } from "astro:schema";
+import { format, isToday, isYesterday, differenceInDays, isSameYear } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -22,5 +22,29 @@ export function getLocalD1DB() {
   } catch (err) {
       throw new Error(`Error ${err}`)
   }
+}
+
+export function formatPostDate(date: Date | number): string {
+  const today = new Date();
+  const postDate = new Date(date);
+  const daysDifference = differenceInDays(today, postDate);
+  const time = format(postDate, "h:mm a"); // e.g., "2:30 PM"
+
+  if (isToday(postDate)) {
+    return `Today at ${time}`;
+  }
+
+  if (isYesterday(postDate)) {
+    return `Yesterday at ${time}`;
+  }
+
+  if (daysDifference >= 2 || daysDifference <= 3) {
+    return daysDifference === 1 ? `${daysDifference} day ago at ${time}` : `${daysDifference} days ago at ${time}`;
+  }
+
+  if (isSameYear(postDate, today)) {
+    return `${format(postDate, "MMM d")} at ${time}`; // "Jan 20 at 2:30 PM"
+  }
+  return `${format(postDate, "MMM d, yyyy")} at ${time}`; // e.g., "Jan 20, 2023 at 2:30 PM"
 }
 
