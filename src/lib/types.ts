79c81e58b24705de5d3c5ec94e9f5b528abdda3f
Ian Mathaiya <iannmathaiya@gmail.com>
2025-01-20 20:23:06 +0300

    feat: get signed urls for friends's media

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,6 @@
+import type { posts } from "@/actions/posts";
+import type { Post, User } from "@/db/schema";
+
 export type FileType = "image/webp" | "image/jpeg" | "image/png" | "video/mp4";
 
 export type Media = {
@@ -24,3 +27,10 @@ export type PendingFriendRequest = {
     status: "pending";
     requester: { id: string; name: string; email: string; image: string | null; };
 };
+
+export type PostAuthor = Pick<User, "id" | "name" | "image">;
+
+export type FriendPost = {
+    post: Post;
+    author: PostAuthor;
+  };
