244d07c8c34111fe5d27f95c9442905192aff900
Ian Mathaiya <iannmathaiya@gmail.com>
2025-01-25 23:36:47 +0300

    updates wrangler toml vars

--- src/pages/api/auth/callback/index.ts
+++ src/pages/api/auth/callback/index.ts
@@ -7,7 +7,9 @@ export async function GET(context: APIContext): Promise<Response> {
 	const state = generateState()
 	const codeVerifier = generateCodeVerifier()
 
-	const redirectURI = context.locals.runtime.env.PROJECT_STATE === 'production' ? 'https://www.pocket-journal.com/api/auth/callback/google' : 'http://localhost:4321/api/auth/callback/google'
+    const { env } = context.locals.runtime;
+
+	const redirectURI = env.PROJECT_STATE === 'production' ? `${env.BASE_URL}/api/auth/callback/google` : 'http://localhost:4321/api/auth/callback/google'
 
 	const google = new Google(import.meta.env.GOOGLE_CLIENT_ID, import.meta.env.GOOGLE_CLIENT_SECRET, redirectURI)
 
